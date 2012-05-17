fc087a57aa776b5cb8c6d86af9abab77f758c320
unknown <splwv5@gmail.com>
2012-05-16 21:48:46 -0500

    addbidder, tablenumber with a table at the bottom

--- addbidder.js
+++ addbidder.js
@@ -1,14 +1,19 @@
-  document.getElementById("Enter").onclick=function addbidder() {
-    var tn = document.getElementById('tablenumber').value
-    var sl = document.getElementById("seatletter").value
-    var fn = document.getElementById('firstname').value
-    var ln = document.getElementById('lastname').value
-    
-    var st = document.getElementById('seattable')
-    var r1c1 = document.getElementById('r1c1')
-  r1c1 = tn
-  alert
-
-
-  }
-
+document.getElementById("Enter").onclick=function addbidder() {
+  var tn = document.getElementById('tablenumber').value
+  var sl = document.getElementById("seatletter").value
+  var fn = document.getElementById('firstname').value
+  var ln = document.getElementById('lastname').value
+  
+  var x=0
+  var test = { tableNum: tn, seatLetter: sl, firstName: fn, lastName: ln }
+  var seatTable = document.getElementById('seattable')
+  
+  var template = "<tr><td>{tableNum}</td>"
+      + "<td>{seatLetter}</td>"
+      + "<td>{firstName}</td>"
+      + "<td>{lastName}</td></tr>"
+  var row = template.replace(/\{([^}]+)\}/g, function (full, name) {
+    return test[name]
+  })
+  seattable.innerHTML = (seatTable.innerHTML + row)
+}
